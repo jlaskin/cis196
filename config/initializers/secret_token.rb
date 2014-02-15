4ee9c6990f20f916b0af8fedce84e6476f878c44
Jeremy Laskin <jlaskin@seas.upenn.edu>
2014-02-15 17:51:54 -0400

    initial commit: updated gemfile and added homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cis196::Application.config.secret_key_base = 'aacfce2ef2afe8e420fdd1c6b7a6f04626f286b4caf5468c6f0eab55d7690684307e7ff064a9b29f7e1d5174370194c6aca13c6e7dc76ea70468121c0587cc95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cis196::Application.config.session_store :cookie_store, key: '_cis196_session'
